40e6e159723e1c64db23bbd36c9177f139370c80
dhanush72608 <dhanush72608@gmail.com>
2026-09-16 13:23:42 +0530

    Fix token lookup key in profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    const token = localStorage.getItem('session_token');
+    const token = localStorage.getItem('token');
     
     if(!token) {
         window.location.href = 'login.html';
